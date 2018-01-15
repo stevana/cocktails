d03e1fac5fb616392e7bc74add91d658f496d00c
Stevan Andjelkovic <stevan.andjelkovic@strath.ac.uk>
2018-01-15 22:34:58 +0100

    Fix link in README and add more cocktails.

--- src/Lib.hs
+++ src/Lib.hs
@@ -180,7 +180,7 @@ makeMenu
   . go
   where
   go _ =
-    "<a href=index.html>all</a> | <a href=ingredient-Gin.html>gin</a> | <a href=ingredient-Whiskey.html>whiskey</a><br />"
+    "<a href=index.html>all</a> | <a href=ingredient-Gin.html>gin</a> | <a href=ingredient-Whiskey.html>whiskey</a> | <a href=ingredient-Rum.html>rum</a> | <a href=ingredient-Champagne.html>champagne</a><br />"
 
 process :: Value -> IO ()
 process v = do
